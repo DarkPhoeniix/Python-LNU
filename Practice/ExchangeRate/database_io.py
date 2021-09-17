a709d3eac15ba8b5973f5da636cabc58dd2b9790
MarynaVovchuk <vovchuk.marina3@gmail.com>
2021-09-17 10:35:17 +0300

    module for databases input/output

--- Practice/ExchangeRate/database_io.py
+++ Practice/ExchangeRate/database_io.py
@@ -1,30 +1,34 @@
 
-import point_forecasting
-import matplotlib.pyplot as plt
+from struct_exchange_rate import ExchangeRateForDay
+from datetime import date
 
-database_UDS = open("database_USD.txt", "r")
-content = database_UDS.readlines()
-my_map = {}
-for i in range(0, len(content), 4):
-    my_map[content[i][0:10]] = {content[i + 1][0:10]: [content[i + 1][11:17], content[i + 1][18:24]],
-                                content[i + 2][0:10]: [content[i + 2][11:17], content[i + 2][18:24]],
-                                content[i + 3][0:15]: [content[i + 3][16:22], content[i + 3][23:29]]}
-names = []
-data_1 = []
-data_2 = []
-data_3 = []
-for i in my_map:
-    names.append(i[0:5])
-    data_1.append(float(my_map.get(i).get('PrivatBank')[0]))
-    data_2.append(float(my_map.get(i).get('PrivatBank')[1]))
-    data_3.append([i[0:2], float(my_map.get(i).get('PrivatBank')[0])])
+# saving date to write in databases
+today_date = date.today()
 
-k, b = point_forecasting.line_by_least_squares_method(data_3)
-r = point_forecasting.forecast_next_point(k, b, 9)
 
-plt.scatter(names, data_1, color='red')
-plt.scatter(names, data_2, color='blue')
-plt.scatter(names, data_3, color='yellow')
+def print_data_to_file(data: ExchangeRateForDay, file_name: str):
+    file = open(file_name, 'a+')
+    file.write(today_date.strftime("%d/%m/%Y") + '\n')
+    for k in data.exchange_rates:
+        file.write(str(k) + '\t')
+        for v in data.exchange_rates.get(k):
+            file.write(str(f"{v:.{3}f}") + '\t')
+        file.write('\n')
+    file.close()
 
-plt.ylim(26, 27.5)
-plt.show()
+
+def read_data_from_file(currency_name: str, file_name: str):
+    file = open(file_name, 'r')
+    content = file.readlines()
+    data_list = []
+    for i in range(0, len(content), 4):
+        currency = ExchangeRateForDay(currency_name)
+        currency.set_buy_rate(content[i + 1][0:10], float(content[i + 1][11:17]))
+        currency.set_sell_rate(content[i + 1][0:10], float(content[i + 1][18:24]))
+        currency.set_buy_rate(content[i + 2][0:10], float(content[i + 2][11:17]))
+        currency.set_sell_rate(content[i + 2][0:10], float(content[i + 2][18:24]))
+        currency.set_buy_rate(content[i + 3][0:15], float(content[i + 3][16:22]))
+        currency.set_sell_rate(content[i + 3][0:15], float(content[i + 3][23:29]))
+        data_list.append(currency)
+
+    return data_list
