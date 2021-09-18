25e26ec30bb830837e83757843eb032f28f56ceb
MarynaVovchuk <vovchuk.marina3@gmail.com>
2021-09-18 15:37:48 +0300

    fixed finding line by least squares method

--- Practice/ExchangeRate/point_forecasting.py
+++ Practice/ExchangeRate/point_forecasting.py
@@ -1,13 +1,13 @@
 
-def line_by_least_squares_method(points):
+def line_by_least_squares_method(points: [[]]):
     matrix = [[0, 0, 0], [0, 0, 0]]
-    for i in range(len(points)):
-        matrix[0][0] += points[i][1]**2
-        matrix[0][1] += points[i][1]
-        matrix[1][0] += points[i][1]
+    for i in range(len(points[0])):
+        matrix[0][0] += points[0][i]**2
+        matrix[0][1] += points[0][i]
+        matrix[1][0] += points[0][i]
         matrix[1][1] += 1
-        matrix[0][2] += int(points[i][0]) * points[i][1]
-        matrix[1][2] += int(points[i][0])
+        matrix[0][2] += float(points[1][i]) * float(points[0][i])
+        matrix[1][2] += float(points[1][i])
 
     det = matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
     det_k = matrix[0][2] * matrix[1][1] - matrix[0][1] * matrix[1][2]
@@ -16,8 +16,3 @@ def line_by_least_squares_method(points):
     b = det_b / det
 
     return k, b
-
-
-
-def forecast_next_point(k, b, x):
-    return x * k + b
