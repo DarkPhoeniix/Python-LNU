fd39d112d7d177d2f958ffcf2ce89b4df74b3b3a
MarynaVovchuk <vovchuk.marina3@gmail.com>
2021-09-23 08:12:04 +0300

    added plot to show exchange rates

--- Practice/ExchangeRate/plot_drawer.py
+++ Practice/ExchangeRate/plot_drawer.py
@@ -1,44 +1,65 @@
 
+from copy import copy
+from datetime import date, datetime, timedelta
 from struct_exchange_rate import ExchangeRateForDay
 from data_parser import parse_data
 import point_forecasting
 import matplotlib.pyplot as plt
 
 
-def draw_plot(exchange_rates: [ExchangeRateForDay]):
-    data = parse_data(exchange_rates)
-    dates = [i.today[0:5] for i in exchange_rates]
+def draw_plot(start_day: date, end_day: date, exchange_rates: [ExchangeRateForDay]):
+    #
+    specified_data = [i for i in exchange_rates if datetime.strptime(i.today, "%d/%m/%Y").date() >= start_day and \
+                                                   datetime.strptime(i.today, "%d/%m/%Y").date() <= end_day]
+    data = parse_data(specified_data)
+    dates = [i.today[0:5] for i in specified_data]  # get off year in dates
+
+    # create plot with 3 subplots for different banks
     fig, axs = plt.subplots(1, 3, figsize=(9, 3))
 
-    need_data = [i + 1 for i in range(len(dates))]
+    # replace dates with natural numbers for easier forecasting
+    numbers = [i + 1 for i in range(len(dates))]
     forecast = [[None] * 2, [None] * 2, [None] * 2]
-    for i in range(3):
-        k, b = point_forecasting.line_by_least_squares_method([need_data, data[i][0]])
-        forecast[i][0] = list((x*k+b) for x in range(len(dates)))
-        k, b = point_forecasting.line_by_least_squares_method([need_data, data[i][1]])
-        forecast[i][1] = list((x*k+b) for x in range(len(dates)))
+    forecast_dates = copy(dates)
+
+    # add next day for forecasting
+    forecast_dates.append((datetime.strptime(dates[-1], '%d/%m') + timedelta(days=1)).strftime('%d/%m'))
+    for i in range(3):  # forecast by least squares method
+        k, b = point_forecasting.line_by_least_squares_method([numbers, data[i][0]])
+        forecast[i][0] = list((x*k+b) for x in range(len(forecast_dates)))
+        k, b = point_forecasting.line_by_least_squares_method([numbers, data[i][1]])
+        forecast[i][1] = list((x*k+b) for x in range(len(forecast_dates)))
+
+    # set titles
     axs[0].set_title('PrivatBank')
     axs[1].set_title('Oschadbank')
     axs[2].set_title('Raiffeisen Bank')
-
+    # cycle for plot drawing
     for i in range(3):
-        axs[i].scatter(dates, data[i][0], color='#006561')
-        axs[i].plot(dates, data[i][0], color='#006561')
-        axs[i].plot(dates, forecast[i][0], color='#5CCDC9')
-        axs[i].scatter(dates, data[i][1], color='#A64A00')
-        axs[i].plot(dates, data[i][1], color='#A64A00')
-        axs[i].plot(dates, forecast[i][1], color='#FFB173')
+        axs[i].plot(dates, data[i][0], 'o', ls='-', ms=4, color='#006561', label='purchase rate')
+        axs[i].plot(forecast_dates, forecast[i][0], color='#5CCDC9')
+        axs[i].plot(forecast_dates[-1], forecast[i][0][-1], 'o', ls='-', ms=4, color='#5CCDC9', label='purchase forecast')
+        axs[i].plot(dates, data[i][1], 'o', ls='-', ms=4, color='#A64A00', label='sale rate')
+        axs[i].plot(forecast_dates, forecast[i][1], color='#FFB173')
+        axs[i].plot(forecast_dates[-1], forecast[i][1][-1], 'o', ls='-', ms=4, color='#FFB173', label='sale forecast')
 
+    # calculate size of each plot
     min_lim = min(min(data[0][0]) - 0.2, min(data[1][0]) - 0.2, min(data[2][0]) - 0.2)
     max_lim = max(max(data[0][1]) + 0.2, max(data[1][1]) + 0.2, max(data[2][1]) + 0.2)
     axs[0].set_ylim(min_lim, max_lim)
     axs[1].set_ylim(min_lim, max_lim)
     axs[2].set_ylim(min_lim, max_lim)
 
-    axs[0].tick_params(labelsize=8)
-    axs[1].tick_params(labelsize=8)
-    axs[2].tick_params(labelsize=8)
+    # set font sizes for axes
+    x_font_size = 6
+    y_font_size = 7
+    x_font_rotation = 60
+    for i in range(3):
+        axs[i].tick_params(axis='x', labelsize=x_font_size, rotation=x_font_rotation)
+        axs[i].tick_params(axis='y', labelsize=y_font_size)
 
-    fig.autofmt_xdate()
+    # set subplots positions
+    plt.subplots_adjust(left=0.08, bottom=0.15, right=0.8, top=0.85)
+    plt.legend(bbox_to_anchor=(1.8, 0.5), loc='center right', borderaxespad=0., fontsize=8)
 
     plt.show()
