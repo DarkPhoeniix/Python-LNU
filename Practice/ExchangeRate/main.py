9e749e6eb68182e8215ecf997a7616e16aa52da1
MarynaVovchuk <vovchuk.marina3@gmail.com>
2021-09-29 09:21:41 +0300

    fixed date validation

--- Practice/ExchangeRate/main.py
+++ Practice/ExchangeRate/main.py
@@ -37,18 +37,25 @@ while True:
         date format: day/month/year
     exit - exit program''')
     elif command_line[0] == 'show':
-        if command_line[1] == 'USD':
-            draw_plot(datetime.datetime.strptime(command_line[2], "%d/%m/%Y").date(),
-                      datetime.datetime.strptime(command_line[3], "%d/%m/%Y").date(),
-                      read_data_from_file('USD', r'database_USD.txt'))
-        if command_line[1] == 'EUR':
-            draw_plot(datetime.datetime.strptime(command_line[2], "%d/%m/%Y").date(),
-                      datetime.datetime.strptime(command_line[3], "%d/%m/%Y").date(),
-                      read_data_from_file('EUR', r'database_EUR.txt'))
-        if command_line[1] == 'RUB':
-            draw_plot(datetime.datetime.strptime(command_line[2], "%d/%m/%Y").date(),
-                      datetime.datetime.strptime(command_line[3], "%d/%m/%Y").date(),
-                      read_data_from_file('RUB', r'database_RUB.txt'))
+        try:
+            start_date = datetime.datetime.strptime(command_line[2], "%d/%m/%Y").date()
+            end_date = datetime.datetime.strptime(command_line[3], "%d/%m/%Y").date()
+            if command_line[1] == 'USD':
+                draw_plot(start_date,
+                          end_date,
+                          read_data_from_file('USD', r'database_USD.txt'))
+            elif command_line[1] == 'EUR':
+                draw_plot(start_date,
+                          end_date,
+                          read_data_from_file('EUR', r'database_EUR.txt'))
+            elif command_line[1] == 'RUB':
+                draw_plot(start_date,
+                          end_date,
+                          read_data_from_file('RUB', r'database_RUB.txt'))
+            else:
+                print("Invalid currency name")
+        except:
+            print("Invalid dates: " + command_line[2] + ' ' + command_line[3])
     elif command_line[0] == 'exit':
         exit()
     else:
